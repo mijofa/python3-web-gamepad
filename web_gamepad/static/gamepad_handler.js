3a21ff10a1436acb459aeee8cefc76c582bcd483
Mike Abrahall <mike@abrahall.id.au>
2020-06-29 11:55:05 +1000

    Check if browser supports gamepads before using them

--- web_gamepad/static/gamepad_handler.js
+++ web_gamepad/static/gamepad_handler.js
@@ -1,6 +1,9 @@
 function gamepad_init() {
-    window.addEventListener("gamepadconnected", gamepad_onOpenGamepad);
-    window.addEventListener("gamepaddisconnected", gamepad_onCloseGamepad);
+    // Does this browser even support gamepads?
+    if !!(navigator.getGamepads) {
+        window.addEventListener("gamepadconnected", gamepad_onOpenGamepad);
+        window.addEventListener("gamepaddisconnected", gamepad_onCloseGamepad);
+    }
 }
 
 function gamepad_onOpenGamepad(evt) {
